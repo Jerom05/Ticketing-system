b78c08554368df467fe271e1a3fadf117e1a07e7
jerom05 <jeromghagra@gmail.com>
2022-05-20 00:26:16 +0600

    add api

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 const app = express()
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000")
 })
